c98dec4353efb6c4ce0034c7cf5a2cba0aae542e
Boby <boby.ms378@gmail.com>
2025-09-29 11:26:44 +0700

    create multiple student object query

--- dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/com/bob/cruddemo/CruddemoApplication.java
+++ dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/com/bob/cruddemo/CruddemoApplication.java
@@ -18,8 +18,24 @@ public class CruddemoApplication {
     public CommandLineRunner commandLineRuner(StudentDAO studentDAO) {
 
         return runner -> {
-            createStudent(studentDAO);
+            // createStudent(studentDAO);
+
+            createMultipleStudents(studentDAO);
         };
+    }
+
+    private void createMultipleStudents(StudentDAO studentDAO) {
+        // Create the student object
+        System.out.println("Creating multiple student object ...");
+        Student tempStudent1 = new Student("John", "Doe", "john.due@gmail.com");
+        Student tempStudent2 = new Student("Miller", "John", "miller@gmail.com");
+        Student tempStudent3 = new Student("Yamada", "Tanaka", "yamada.tan@gmail.com");
+
+        // Save the student objects
+        System.out.println("Saving the students data ...");
+        studentDAO.save(tempStudent1);
+        studentDAO.save(tempStudent2);
+        studentDAO.save(tempStudent3);
     }
 
     private void createStudent(StudentDAO studentDAO) {
