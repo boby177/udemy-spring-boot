f089a295c09c90d6736de7e430d8f423a316f12c
Boby <boby.ms378@gmail.com>
2025-10-20 10:02:31 +0700

    spring mvc form data binding

---
+++ dev-spring-boot/06-spring-boot-mvc/02-thymeleafdemo-form-data-binding/src/main/java/com/bob/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,60 @@
+package com.bob.springboot.thymeleafdemo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+    // new controller method to show initial HTML Form
+    @GetMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+    // need a controller method to process the HTML Form
+    @RequestMapping("/processForm")
+    public String processForm() {
+        return "helloworld";
+    }
+
+    // need a controller method to read form data
+    @RequestMapping("/processFormVersion2")
+    public String letShoutDude(HttpServletRequest request, Model model) {
+
+        // Read the request params from the html form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all uppercase
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @PostMapping("/processFormVersion3")
+    public String processFormVersion3(@RequestParam("studentName") String theName,
+                                      Model model) {
+
+        // convert the data to all uppercase
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Hey My Friend! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+}
---
+++ dev-spring-boot/06-spring-boot-mvc/02-thymeleafdemo-form-data-binding/src/main/java/com/bob/springboot/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,34 @@
+package com.bob.springboot.thymeleafdemo.controller;
+
+import com.bob.springboot.thymeleafdemo.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+
+    @GetMapping("/showStudentForm")
+    public String showForm(Model theModel) {
+
+        // Create a new student object
+        Student theStudent = new Student();
+
+        // Ad student object to the model
+        theModel.addAttribute("student", theStudent);
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processStudent(@ModelAttribute("student") Student theStudent) {
+
+        // Log the input data
+        System.out.println("theStudent: " + theStudent.getFirstName() + " "
+                + theStudent.getLastName());
+
+        return "student-confirmation";
+    }
+}
