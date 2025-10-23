ee4097628c93028e54371f158dabd942b87cec9b
Boby <boby.ms378@gmail.com>
2025-10-23 10:50:40 +0700

    new validations integer type is required and implement custom validations

--- dev-spring-boot/06-spring-boot-mvc/03-thymeleafdemo-validations/src/main/java/com/bob/springdemo/mvc/Customer.java
+++ dev-spring-boot/06-spring-boot-mvc/03-thymeleafdemo-validations/src/main/java/com/bob/springdemo/mvc/Customer.java
@@ -9,9 +9,10 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName = "";
 
+    @NotNull(message = "is required")
     @Min(value = 0, message = "The value cannot less than 0")
     @Max(value = 10, message = "The value cannot greater than 10")
-    private int freePasses;
+    private Integer freePasses;
 
     // Regular Expression validations
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "Only 5 characters")
@@ -33,11 +34,11 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
