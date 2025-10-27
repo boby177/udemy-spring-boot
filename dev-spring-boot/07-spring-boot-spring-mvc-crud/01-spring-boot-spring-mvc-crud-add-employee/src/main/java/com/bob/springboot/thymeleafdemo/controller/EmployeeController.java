4b1a41007aec7cdc13431a6539de1e16d967f861
Boby <boby.ms378@gmail.com>
2025-10-27 16:47:29 +0700

    new endpoint save to process new employee and order by sorted last name data

--- dev-spring-boot/07-spring-boot-spring-mvc-crud/01-spring-boot-spring-mvc-crud-add-employee/src/main/java/com/bob/springboot/thymeleafdemo/controller/EmployeeController.java
+++ dev-spring-boot/07-spring-boot-spring-mvc-crud/01-spring-boot-spring-mvc-crud-add-employee/src/main/java/com/bob/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.bob.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -32,12 +34,21 @@ public class EmployeeController {
     }
 
     @GetMapping("/showFormForAdd")
-    public String showFOrmForAdd(Model theModel) {
+    public String showFormForAdd(Model theModel) {
         // Create model attribute to bind form data
         Employee theEmployee = new Employee();
 
         theModel.addAttribute("employee", theEmployee);
 
         return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmloyee) {
+        // Save the new employee
+        employeeService.save(theEmloyee);
+
+        // Use a redirect to prevent duplicate submission
+        return "redirect:/employees/list";
     }
 }
