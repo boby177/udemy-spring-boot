cde20afb2e56535af8ff37fdbe93415bdc457aac
Boby <boby.ms378@gmail.com>
2025-10-28 15:28:19 +0700

    implement update employee feature

--- dev-spring-boot/07-spring-boot-spring-mvc-crud/01-spring-boot-spring-mvc-crud-add-employee/src/main/java/com/bob/springboot/thymeleafdemo/controller/EmployeeController.java
+++ dev-spring-boot/07-spring-boot-spring-mvc-crud/01-spring-boot-spring-mvc-crud-add-employee/src/main/java/com/bob/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -4,10 +4,7 @@ import com.bob.springboot.thymeleafdemo.entity.Employee;
 import com.bob.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,6 +37,18 @@ public class EmployeeController {
 
         theModel.addAttribute("employee", theEmployee);
 
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/showFormUpdate")
+    public String showFormUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+        // Get the employee from the service
+        Employee theEmployee = employeeService.findById(theId);
+
+        // Set employee in the model to prepopulate the form
+        theModel.addAttribute("employee", theEmployee);
+
+        // Send over to our form
         return "employees/employee-form";
     }
 
