e7dc63f2737b16d848fe37df08b193a5d8f1c03a
Boby <boby.ms378@gmail.com>
2025-11-21 11:12:25 +0700

    implement find instructor by id and delete instructor by id

--- dev-spring-boot/09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/bob/cruddemo/CruddemoApplication.java
+++ dev-spring-boot/09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/bob/cruddemo/CruddemoApplication.java
@@ -19,8 +19,31 @@ public class CruddemoApplication {
     public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 
         return runner -> {
-            createInsructor(appDAO);
+            // createInsructor(appDAO);
+            // findInstructor(appDAO);
+            deleteInstructor(appDAO);
         };
+    }
+
+    private void deleteInstructor(AppDAO appDAO) {
+
+        int theId = 2;
+        System.out.println("Deleting instructor id: " + theId);
+
+        appDAO.deleteInstructorById(theId);
+
+        System.out.println("Instructor Deleted!");
+    }
+
+    private void findInstructor(AppDAO appDAO) {
+
+        int theId = 1;
+        System.out.println("Finding instructor id: " + theId);
+
+        Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+        System.out.println("tempInstructor: " + tempInstructor);
+        System.out.println("the associate instructorDetail only: " + tempInstructor.getInstructorDetail());
     }
 
     private void createInsructor(AppDAO appDAO) {
