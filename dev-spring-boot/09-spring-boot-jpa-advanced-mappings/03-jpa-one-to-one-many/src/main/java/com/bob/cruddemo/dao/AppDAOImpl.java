3d9a84111698354bf925dc1c95d02fe16c941e58
Boby <boby.ms378@gmail.com>
2025-12-17 14:16:00 +0700

    implement join fetch on table courses and instructor detail

--- dev-spring-boot/09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-one-many/src/main/java/com/bob/cruddemo/dao/AppDAOImpl.java
+++ dev-spring-boot/09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-one-many/src/main/java/com/bob/cruddemo/dao/AppDAOImpl.java
@@ -78,4 +78,21 @@ public class AppDAOImpl implements AppDAO {
         List<Course> courses = query.getResultList();
         return courses;
     }
+
+    @Override
+    public Instructor findInstructorByIdJoinFetch(int theId) {
+
+        // Create query
+        TypedQuery<Instructor> query = entityManager.createQuery(
+                "select i from Instructor i "
+                        + "JOIN FETCH i.courses "
+                        + "JOIN FETCH i.instructorDetail "
+                        + "where i.id = :data", Instructor.class
+        );
+        query.setParameter("data", theId);
+
+        // Execute query
+        Instructor instructor = query.getSingleResult();
+        return instructor;
+    }
 }
