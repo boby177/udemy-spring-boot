084601d42f99e46cabf3f4ebd7fc851355995e57
Boby <boby.ms378@gmail.com>
2026-01-15 11:12:25 +0700

    refactor course entity add review join column

--- dev-spring-boot/09-spring-boot-jpa-advanced-mappings/04-jpa-uni-directional/src/main/java/com/bob/cruddemo/entity/Course.java
+++ dev-spring-boot/09-spring-boot-jpa-advanced-mappings/04-jpa-uni-directional/src/main/java/com/bob/cruddemo/entity/Course.java
@@ -2,6 +2,9 @@ package com.bob.cruddemo.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "course")
 public class Course {
@@ -19,6 +22,10 @@ public class Course {
             CascadeType.DETACH, CascadeType.REFRESH})
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private List<Review> reviews;
 
     public Course() {
 
@@ -52,6 +59,24 @@ public class Course {
 
     public void setInstructor(Instructor instructor) {
         this.instructor = instructor;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    // Add a convenience method
+    public void addReview(Review theReview) {
+
+        if(reviews == null) {
+            reviews = new ArrayList<>();
+        }
+
+        reviews.add(theReview);
     }
 
     // Automatically generate toString()
