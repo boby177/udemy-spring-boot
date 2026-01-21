7234d42da5d81d2863260aaff3e53ad0fbcad47d
Boby <boby.ms378@gmail.com>
2026-01-21 14:09:09 +0700

    config many to many map course to student

--- dev-spring-boot/09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/bob/cruddemo/entity/Student.java
+++ dev-spring-boot/09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/bob/cruddemo/entity/Student.java
@@ -2,6 +2,9 @@ package com.bob.cruddemo.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "student")
 public class Student {
@@ -19,6 +22,12 @@ public class Student {
 
     @Column(name = "email")
     private String email;
+
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+                    CascadeType.DETACH, CascadeType.REFRESH},
+            mappedBy = "students")
+    private List<Course> courses;
 
     public Student() {
 
@@ -62,6 +71,25 @@ public class Student {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    // Add convenience method
+    public void addCourse(Course theCourse) {
+
+        if (courses == null) {
+            courses = new ArrayList<>();
+        }
+
+        courses.add(theCourse);
+        theCourse.addStudent(this);
     }
 
     // Generate toString()
